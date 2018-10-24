f759bd2da31ae5849ef30df4b4e36cea0088be9a
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2018-10-24 11:32:06 +0200

    dev

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\ButerMenuBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Config\ContainerBuilder;
+use Contao\ManagerPlugin\Config\ExtensionPluginInterface;
+use HeimrichHannot\ButterMenuBundle\ContaoButterMenuBundle;
+use HeimrichHannot\EncoreBundle\HeimrichHannotContaoEncoreBundle;
+use HeimrichHannot\UtilsBundle\Container\ContainerUtil;
+
+class Plugin implements BundlePluginInterface, ExtensionPluginInterface
+{
+    /**
+     * Gets a list of autoload configurations for this bundle.
+     *
+     * @param ParserInterface $parser
+     *
+     * @return ConfigInterface[]
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(ContaoButterMenuBundle::class)->setLoadAfter(
+                [
+                    ContaoCoreBundle::class,
+                    HeimrichHannotContaoEncoreBundle::class,
+                ]
+            ),
+        ];
+    }
+
+
+    /**
+     * Allows a plugin to override extension configuration.
+     *
+     * @param string           $extensionName
+     * @param array            $extensionConfigs
+     * @param ContainerBuilder $container
+     *
+     * @return
+     */
+    public function getExtensionConfig($extensionName, array $extensionConfigs, ContainerBuilder $container)
+    {
+        $extensionConfigs = ContainerUtil::mergeConfigFile(
+            'huh_encore',
+            $extensionName,
+            $extensionConfigs,
+            __DIR__.'/../Resources/config/config_encore.yml'
+        );
+
+        return $extensionConfigs;
+    }
+}
---
+++ src/DependencyInjection/ButterMenuExtension.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @author  Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
+ * @license http://www.gnu.org/licences/lgpl-3.0.html LGPL
+ */
+
+namespace HeimrichHannot\ButterMenuBundle\DependencyInjection;
+
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ButterMenuExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('services.yml');
+    }
+}
